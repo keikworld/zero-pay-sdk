c0cc651019201bad639c2b3972def8fefb82e86f
Keik World Project <159653689+keikworld@users.noreply.github.com>
2025-10-01 14:46:43 -0500

    Refactor canvasForFactor to be a Composable function

--- sdk/src/androidMain/kotlin/com/zeropay/sdk/ZeroPay.kt
+++ sdk/src/androidMain/kotlin/com/zeropay/sdk/ZeroPay.kt
@@ -1,5 +1,7 @@
 package com.zeropay.sdk
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.zeropay.sdk.factors.FactorCanvasFactory
 import com.zeropay.sdk.factors.FactorRegistry
 
@@ -8,19 +10,12 @@ object ZeroPay {
     fun availableFactors(context: android.content.Context) =
         FactorRegistry.availableFactors(context)
 
-    // The FactorCanvasFactory.CanvasForFactor is a @Composable function.
-    // A regular object function like ZeroPay.canvasForFactor cannot directly return or execute it
-    // in a way that it becomes part of a Composable UI tree.
-    // It should also be @Composable if it's intended to be used as a Composable.
-    // Or, it simply acts as a passthrough to call the other Composable.
-    // For now, I will keep the structure as you provided, but this might need adjustment
-    // depending on how it's called.
-    // If it's meant to be a Composable wrapper:
-    // @Composable
-    // fun canvasForFactor(factor: Factor, onDone: (ByteArray) -> Unit, modifier: Modifier = Modifier) {
-    //     FactorCanvasFactory.CanvasForFactor(factor, onDone, modifier)
-    // }
-    // If it's just a direct call (and the caller handles composability):
-    fun canvasForFactor(factor: Factor, onDone: (ByteArray) -> Unit) =
-        FactorCanvasFactory.CanvasForFactor(factor, onDone)
+    @Composable
+    fun canvasForFactor(
+        factor: Factor,
+        onDone: (ByteArray) -> Unit,
+        modifier: Modifier = Modifier
+    ) {
+        FactorCanvasFactory.CanvasForFactor(factor, onDone, modifier)
+    }
 }
